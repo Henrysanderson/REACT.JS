85b8a60c007fc94be73a13eb56c46ef408f7f3bc
Henry Sanderson Viyuyi <henryviyuyi2@gmail.com>
2022-12-12 00:48:19 +0200

    Second commit

--- ReactProject/src/App.jsx
+++ ReactProject/src/App.jsx
@@ -1,11 +1,43 @@
 import styles from "./style";
 
+// IMPORTING COMPONENTS
+import {Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials, Hero} from './components';
+
 const App = () => (
   <div className='bg-primary w-full overflow-hidden'>
+
+{/* NAVBAR SECTION */}
    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Navbar
+
+        <Navbar />
+
       </div>
+   </div>
+
+{/* HERO SECTION */}
+   <div className={`bg-primary ${styles.flexStart}`}>
+    <div className={`${styles.boxWidth}`}>
+
+        <Hero />
+
+    </div>
+   </div>
+
+{/* ALL THE OTHER SECTIONS */}
+   <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`${styles.boxWidth}`}>
+
+    <Billing />
+    <CardDeal />
+    <Business />
+    <Clients />
+    <CTA />
+    <Stats />
+    <Footer />
+    <Testimonials />
+
+    </div>
    </div>
   </div>
 );
